2566eed4887f5e827e38711d4603efe2406f9170
Nhan Cao <caovannhan2002@gmail.com>
2016-02-05 02:05:36 +0530

    update plugins

--- plugins/redmine_bt-master/lib/bootstrap_hooks.rb
+++
@@ -1,7 +0,0 @@
-class BootstrapHooks < Redmine::Hook::ViewListener
-  render_on(:view_layouts_base_html_head, :partial => 'hooks/css')
-  render_on(:view_layouts_base_body_bottom, :partial => 'hooks/js')
-end
-
-
-
